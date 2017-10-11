2ef80edf7fc2dfaf93908de852d4003025f19be7
jkrmnj <jkrmnj465@gmail.com>
2017-10-10 19:10:25 -0600

    Actually commit the things from earlier

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cisplay',
+      version='0.1',
+      description='Visplay',
+      url='https://github.com/ColoradoSchoolOfMines/visplay',
+      author='CSM ACM',
+      author_email='Not yet',
+      license='MIT',
+      packages=['visplay'],
+      zip_safe=False)
