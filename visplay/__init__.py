219d5266fe73433892f02b7795398d73ed8aa012
jkrmnj <jkrmnj465@gmail.com>
2017-11-14 20:50:13 -0700

    Scanning a directory

--- visplay/__init__.py
+++ visplay/__init__.py
@@ -1,3 +1,23 @@
 import mpv
-player = mpv.MPV(ytdl=True)
-player.play('https://www.youtube.com/watch?v=Ro6r15wzp2o')
+from os import listdir
+
+def my_log(loglevel, component, message):
+      print('[{}] {}: {}'.format(loglevel, component, message))
+
+def main():
+      #player = mpv.MPV(log_handler=my_log, ytdl=True, input_default_bindings=True, input_vo_keyboard=True)
+      player = mpv.MPV(log_handler=my_log)
+      #player = mpv.MPV()
+      player.fullscreen = True
+      stillRunning = True
+
+      while stillRunning:
+            for v in findVideos('/home/jack/Videos/visplay'):
+                  player.play(v)
+                  player.wait_for_playback()
+
+def findVideos(path):
+      return map(lambda a: path + "/" + a, listdir(path))
+
+if __name__ == "__main__":
+      main()
