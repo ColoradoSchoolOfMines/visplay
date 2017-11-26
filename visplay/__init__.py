91ec3b311d826454e10d4837aee3d6c397e0c5ed
Robby Zampino <robozman@gmail.com>
2017-11-26 01:28:00 -0700

    modified run loop to check for files to play, and to use generic user path. changed find_videos to return a list comprehension instead of a lambda

--- visplay/__init__.py
+++ visplay/__init__.py
@@ -1,23 +1,47 @@
-import mpv
+'visplay: from os import listdir'
+
 from os import listdir
+from os import path
+import mpv
+
 
 def my_log(loglevel, component, message):
-      print('[{}] {}: {}'.format(loglevel, component, message))
+    'my_log; acts as the log handler for the mpv object'
+    print('[{}] {}: {}'.format(loglevel, component, message))
+
 
 def main():
-      #player = mpv.MPV(log_handler=my_log, ytdl=True, input_default_bindings=True, input_vo_keyboard=True)
-      player = mpv.MPV(log_handler=my_log)
-      #player = mpv.MPV()
-      player.fullscreen = True
-      stillRunning = True
+    'main: main entrypoint for program when run standalone'
+#     player = mpv.MPV(log_handler=my_log, ytdl=True,
+#                input_default_bindings=True, input_vo_keyboard=True)
 
-      while stillRunning:
-            for v in findVideos('/home/jack/Videos/visplay'):
-                  player.play(v)
-                  player.wait_for_playback()
+    still_running = True
 
-def findVideos(path):
-      return map(lambda a: path + "/" + a, listdir(path))
+    video_path = path.expanduser('~/Videos/visplay')
+
+    while still_running:
+        print('Looking for videos in %s' % (video_path))
+        video_map = find_videos(video_path)
+        print('Found %d videos!' % (len(video_map)))
+
+        if not video_map:
+            print("Exiting...")
+            exit(1)
+
+        player = mpv.MPV(log_handler=my_log)
+    #     player = mpv.MPV()
+        player.fullscreen = True
+
+        for video in video_map:
+            player.play(video)
+            player.wait_for_playback()
+
+
+def find_videos(video_path):
+    'findVideos: finds all video files in video path'
+    return [video_path + '/' + file for file in listdir(video_path)]
+    # return map(lambda a: video_path + "/" + a, listdir(video_path))
+
 
 if __name__ == "__main__":
-      main()
+    main()
