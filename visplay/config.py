ddb01446fb1a37195834b2ec406bd60e7a3400c7
Robby Zampino <robozman@gmail.com>
2018-03-13 20:41:21 -0600

    Added new example configs and added config.py missing from last commit

---
+++ visplay/config.py
@@ -0,0 +1,28 @@
+import yaml
+from os import environ, path, makedirs
+from argparse import ArgumentParser
+
+
+def load_config_yaml():
+
+    config_folder = environ.get('XDG_CONFIG_HOME') or path.join(
+        environ.get('HOME'), '.config')
+    config_folder = path.join(config_folder, 'visplay')
+    if not path.exists(config_folder):
+        makedirs(config_folder)
+
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--config',
+                        dest='config',
+                        type=open,
+                        default=path.join(config_folder, 'config.yaml'),
+                        help='Specify a custom configuration file to load.')
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        print('Error parsing arguments:', e)
+        exit(1)
+
+    config = yaml.load(args.config)
+
+    return config
