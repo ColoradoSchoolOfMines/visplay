7ab655be7bf3d997c527753b5a8eb45d9093c2bb
Sumner Evans <sumner.evans98@gmail.com>
2018-04-03 22:02:57 -0600

    #13 prevent explosion when config file does not exist

--- visplay/config.py
+++ visplay/config.py
@@ -1,26 +1,35 @@
-import yaml
-from os import environ, path, makedirs
 from argparse import ArgumentParser
+from os import environ, makedirs, path
+
+import yaml
 
 
 def load_config_yaml():
-    '''load_config_yaml: loads config.yaml based on provided arguments
-        or default location'''
+    """Loads config.yaml based on provided arguments or default location."""
 
-    config_folder = environ.get('XDG_CONFIG_HOME') or path.join(
-        environ.get('HOME'), '.config')
+    config_folder = (environ.get('XDG_CONFIG_HOME') or
+                     path.join(environ.get('HOME'), '.config'))
     config_folder = path.join(config_folder, 'visplay')
     if not path.exists(config_folder):
         makedirs(config_folder)
 
+    default_config = path.join(config_folder, 'config.yaml')
+
     parser = ArgumentParser()
-    parser.add_argument('-c', '--config',
-                        dest='config',
-                        type=open,
-                        default=path.join(config_folder, 'config.yaml'),
-                        help='Specify a custom configuration file to load.')
+    parser.add_argument(
+        '-c',
+        '--config',
+        dest='config',
+        type=open,
+        default=default_config,
+        help='Specify a custom configuration file to load.',
+    )
+
     try:
         args = parser.parse_args()
+    except FileNotFoundError:
+        print(f'ERROR: Configuration file not found at {default_config}')
+        exit(1)
     except Exception as e:
         print('Error parsing arguments:', e)
         exit(1)
