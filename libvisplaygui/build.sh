a1ac2252f00d5dbf4e7bd592d8323f31c547900a
Robby Zampino <robozman@gmail.com>
2018-06-02 22:38:38 -0600

    Fixed docker building and addad docker to Travis config

--- libvisplaygui/build.sh
+++ libvisplaygui/build.sh
@@ -3,12 +3,12 @@ mkdir build
 cd build
 
 if [ "$1" == "distclean" ]; then
-    qmake-qt5 ../visplay-gui.pro
+    qmake ../visplay-gui.pro
     make distclean
     cd ..
     rm -r build
     exit
 fi
 
-qmake-qt5 ../visplay-gui.pro
+qmake ../visplay-gui.pro
 make
